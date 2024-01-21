a9d02ece6766d12821ab521e394bb15d743a6ef2
Alex Hiriavenko <martmarchmartmarch@gmail.com>
2024-01-22 00:50:22 +0200

    fix: prettier and linter rules

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,13 +5,13 @@ const { stdin, stdout } = require('process');
 
 const filePath = path.join(__dirname, 'check-this.txt');
 
-// флаг "a" - файл будет открыт для записи, если файл не существует, он будет создан. Если файл существует, данные будут добавлены в конец файла.
+// флаг 'a' - файл будет открыт для записи, если файл не существует, он будет создан. Если файл существует, данные будут добавлены в конец файла.
 const fileStream = fs.createWriteStream(filePath, { flags: 'a' });
 
 const rl = readline.createInterface(stdin);
 
 const startMessage =
-  "Hello, write some text here. You will find it in 'check-this.txt' file\n";
+  'Hello, write some text here. You will find it in "check-this.txt" file\n';
 const endMessage = 'Thank you for review this task.';
 
 stdout.write(startMessage);
